582df1c87d47f0d19c8ce8b3b234c9521a4862ee
Kamil Bąk <sebastian.bak.23@gmail.com>
2020-06-03 22:58:15 +0200

    v0.1

---
+++ Frankenstein’s Samurai Rally/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    private Rigidbody2D rb;
+    public Vector2 movement;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+    }
+}
---
+++ Frankenstein’s Samurai Rally/Assets/Scripts/TouchController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+    private float screenWidth;
+    public float speed = 10f;
+    private Vector3 velocity;
+
+    void Start()
+    {
+        screenWidth = Screen.height;
+        velocity = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPos.z = 0;
+
+            if (touch.position.y < screenWidth / 2 && touch.phase == TouchPhase.Began)
+            {
+                if (velocity.y > -2)
+                {
+                    velocity += Vector3.down;
+                }
+            }
+
+            if (touch.position.y > screenWidth / 2 && touch.phase == TouchPhase.Began)
+            {
+                if (velocity.y < 0)
+                {
+                    velocity += Vector3.up;
+                }
+            }
+
+        }
+
+        transform.position = Vector3.Lerp(transform.position, velocity, speed * Time.deltaTime);
+
+    }
+}
