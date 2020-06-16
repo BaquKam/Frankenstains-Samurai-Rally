ce934445637ae69961a5792a0b049fced0a03e96
Kamil Bąk <sebastian.bak.23@gmail.com>
2020-06-16 18:56:32 +0200

    Revert "Revert "v5.1""
    This reverts commit 2fd70935cc07960f074bc17094806c34ba585c70.

--- Frankenstein’s Samurai Rally/Assets/Scripts/BonusSpawner.cs
+++ Frankenstein’s Samurai Rally/Assets/Scripts/BonusSpawner.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class BonusSpawner : MonoBehaviour
 {
     public GameObject[] bonuses;
+    public int minDelay;
+    public int maxDelay;
+
     private float delay;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        delay = Random.Range(minDelay, maxDelay);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        delay -= Time.deltaTime;
+        if(delay <= 0)
+        {
+            delay = Random.Range(minDelay, maxDelay);
+            SpawnBonus();
+        }
+    }
+
+    void SpawnBonus()
+    {
+        Instantiate(bonuses[(int)Random.Range(0,2)], new Vector3(7.7f, Random.Range(-0.28f, -2.79f), 0), Quaternion.identity);
     }
 }
