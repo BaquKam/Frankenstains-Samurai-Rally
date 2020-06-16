9d20ae6b0e458324c8a98bab1f6cd26e370d28da
Kamil Bąk <sebastian.bak.23@gmail.com>
2020-06-16 14:10:52 +0200

    v0.5
    Add BonusShield,
    ~problem with the glow for a bonus

--- Frankenstein’s Samurai Rally/Assets/Scripts/Bonuses.cs
+++ Frankenstein’s Samurai Rally/Assets/Scripts/Bonuses.cs
@@ -4,21 +4,46 @@ using UnityEngine;
 
 public class Bonuses : MonoBehaviour
 {
-    [Header("Type obiect")]
+    [Header("Type of bonus")]
     public bool isShield;
-    
 
+    [Header("Bonuses Settings")]
+    public float bonusSpeed = 10f;
 
+    [Header("Shield Settings")]
+    public GameObject shield;
+    public GameObject enemy;
+    private GameObject player;
+    private Vector3 playerPos;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.gameObject.transform.Translate(new Vector3(-1, 0, 0) * bonusSpeed * Time.deltaTime);        
+    }
+
+    private void OnTriggerEnter2D(Collider2D obj)
+    {
+        if(obj.gameObject.tag == "Player")
+        {
+            if(isShield == true)
+            {
+                player = GameObject.FindWithTag("Player");
+                obj.gameObject.tag = "Shield";
+
+                enemy.gameObject.tag = "Proof";
+
+                playerPos = player.transform.position;
+                playerPos.z = -0.1f;
+                GameObject shieldObj = (GameObject)Instantiate(shield, playerPos, Quaternion.identity);
+                shieldObj.transform.parent = player.transform;
+                Destroy(this.gameObject);
+            }
+        }
+        else if (obj.gameObject.tag == "ObjectDestroyer")
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
---
+++ Frankenstein’s Samurai Rally/Assets/Scripts/Shield.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    public float duration;
+    public GameObject enemy;
+
+    void Update()
+    {
+        duration -= Time.deltaTime;
+        if(duration <= 0)
+        {
+            this.gameObject.transform.parent.tag = "Player";
+            enemy.gameObject.tag = "Hurdle";
+            Destroy(this.gameObject);
+        }
+    }
+}
