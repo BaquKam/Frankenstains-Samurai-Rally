4f11e16ff8ebdf97d9960323107135914ad801ee
Kamil Bąk <sebastian.bak.23@gmail.com>
2020-06-19 13:13:52 +0200

    v0.8.4

--- Frankenstein’s Samurai Rally/Assets/Scripts/HurdleSpawner.cs
+++ Frankenstein’s Samurai Rally/Assets/Scripts/HurdleSpawner.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D.Animation;
+using UnityEngine.UIElements;
 
 public class HurdleSpawner : MonoBehaviour
 {
     public float hurdleSpawnDelay = 2f;
-    public GameObject hurdle;
+    public GameObject hurdle0;
+    public GameObject hurdle1;
+    public GameObject hurdle2;
+
+    private Vector3 playerPos;
+    public GameObject player;
+
     private float spawnDelay;
 
     private float[] pathArray;
@@ -29,12 +37,23 @@ public class HurdleSpawner : MonoBehaviour
             spawnHurdle();
             spawnDelay = hurdleSpawnDelay;
         }
+
+        playerPos = player.transform.position;
     }
 
     void spawnHurdle()
     {
         int lane = Random.Range(0, 3);
-
-        Instantiate(hurdle, new Vector3(8.1f, pathArray[lane], 0), Quaternion.identity);
+        if(lane == 0)
+        {
+            Instantiate(hurdle0, new Vector3(8.1f, pathArray[lane], 0), Quaternion.identity);
+        } else if(lane == 1 || lane == 2)
+        {
+            Instantiate(hurdle1, new Vector3(8.1f, pathArray[lane], 0), Quaternion.identity);
+        }/*else if(lane == 1 && playerPos.y <= 1.5f)
+        {
+            Debug.Log("jest ponizej");
+            Instantiate(hurdle2, new Vector3(8.1f, pathArray[lane], 0), Quaternion.identity);
+        }*/
     }
 }
